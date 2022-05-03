fa258e90b3f0135d01c973187c295b81b22f7ef1
tak-ka3 <hi.ta9.23.333@gmail.com>
2022-05-03 10:30:14 +0900

    IMPROVE: make stack

--- 9cc.c
+++ 9cc.c
@@ -209,6 +209,39 @@ Node *expr() {
   }
 }
 
+void gen(Node *node) {
+  if (node->kind == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+  
+  switch (node->kind) {
+    case ND_ADD:
+      printf("  add rax, rdi\n");
+      break;
+    case ND_SUB:
+      printf("  sub rax, rdi\n");
+      break;
+    case ND_MUL:
+      printf("  imul rax, rdi\n");
+      break;
+    case ND_DIV:
+      printf("  cqo\n");
+      printf("  idiv rdi\n");
+      break;
+    default:
+      error("error node kind\n");
+      break;
+  }
+
+  printf("  push rax\n");
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     error_at(token->str, "引数の個数が正しくありません");
