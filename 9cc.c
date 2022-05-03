9bff5758e0f4f54d123f0e97b7eb460ac2c73291
tak-ka3 <hi.ta9.23.333@gmail.com>
2022-05-03 10:22:25 +0900

    IMPROVE: change token to tree by parsing

--- 9cc.c
+++ 9cc.c
@@ -12,6 +12,23 @@ typedef enum {
   TK_EOF, // 入力の終わりを表すトークン
 } TokenKind;
 
+// 抽象構文木のノードの種類
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM, // 整数
+} NodeKind;
+
+// struct TokenをTokenという名前の型で定義している
+// わざわざ型を定義するときに、struct Token *t; などと定義しなくていいようになる
+/* typedef struct Token{
+  ~~~
+}Token;
+という書き方も一般的
+*/
+// 入力をトークン列に変換する
 typedef struct Token Token;
 
 struct Token {
@@ -20,6 +37,21 @@ struct Token {
   int val; // kindがTK_NUMの場合、その数値
   char *str; // トークン文字列
 };
+
+// トークン列をパースして抽象構文木に変換する
+typedef struct Node Node;
+
+struct Node {
+  NodeKind kind; // ノードの型
+  Node* lhs; // 左辺
+  Node* rhs; // 右辺
+  int val; // 整数だった場合の型
+};
+
+// プロトタイプ宣言
+Node *expr();
+Node *mul();
+Node *primary();
 
 // 現在着目しているトークン
 Token *token;
@@ -123,6 +155,58 @@ Token *tokenize(char *p) {
   }
   new_token(TK_EOF, cur, p);
   return head.next;
+}
+
+Node *new_node(NodeKind kind, Node* lhs, Node* rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *primary() {
+  if (consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+  return new_node_num(expect_number()); // 数値はここでのみ返される
+}
+
+Node *mul() {
+  Node *node = primary();
+
+  for (;;) {
+    if (consume('*')) {
+      node = new_node(ND_MUL, node, primary());
+    } else if (consume('/')) {
+      node = new_node(ND_DIV, node, primary());
+    } else {
+      return node;
+    }
+  }
+}
+
+Node *expr() {
+  Node *node = mul();
+
+  for (;;) {
+    if (consume('+')) {
+      node = new_node(ND_ADD, node, mul());
+    } else if (consume('-')) {
+      node = new_node(ND_SUB, node, mul());
+    } else {
+      return node;
+    }
+  }
 }
 
 int main(int argc, char **argv) {
